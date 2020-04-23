0c62a62e8d1f325cd510816d6dc3df7571e9246a
René Bodack <rene.bodack@kreuzwerker.de>
2020-04-23 13:43:04 +0200

    Break it like you mean it

--- src/product/product.js
+++ src/product/product.js
@@ -1,6 +1,6 @@
 class Product {
     constructor(id, type, name, version) {
-        this.id = id;
+        this.id = uuid;
         this.type = type;
         this.name = name;
         this.version = version;
